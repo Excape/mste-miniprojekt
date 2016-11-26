0f9eff6ca5d39449ca3ffd71cfeaea55e17176c1
Raphael Zimmermann <mail@raphael.li>
2016-11-26 16:21:50 +0100

    Implement Business Layer

--- AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
+++ AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
@@ -29,21 +29,102 @@ namespace AutoReservation.BusinessLayer.Testing
         }
         
         [TestMethod]
+        public void CRUDAutoTest()
+        {
+            // Create
+            Assert.IsNull(Target.loadAuto(4));
+            var auto = new LuxusklasseAuto { Marke = "VW", Tagestarif = 150, Basistarif = 100 };
+            Target.insertAuto(auto);
+            var timestamp = auto.RowVersion;
+            Assert.AreEqual(4, auto.Id);
+
+            // Read
+            Assert.IsNotNull(Target.loadAuto(4));
+            
+            // Update
+            auto.Basistarif = 4993;
+            Target.updateAuto(auto);
+            Assert.AreNotEqual(timestamp, auto.RowVersion);
+
+            // Delete
+            Target.deleteAuto(auto);
+            Assert.IsNull(Target.loadAuto(4));
+
+        }
+
+        [TestMethod]
         public void UpdateAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var auto1 = Target.loadAuto(1);
+            var auto2 = Target.loadAuto(1);
+            Assert.AreNotEqual(auto1, auto2);
+
+            auto1.Marke = "A";
+            auto2.Marke = "B";
+
+            Target.updateAuto(auto1);
+
+            try
+            {
+                Target.updateAuto(auto2);
+                Assert.Fail("Was able to override!");
+            }catch(LocalOptimisticConcurrencyException<Auto>)
+            {
+                // Everything OK
+            }
+
+            Assert.AreEqual("A", Target.loadAuto(1).Marke);
         }
+
 
         [TestMethod]
         public void UpdateKundeTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var kunde1 = Target.loadKunde(1);
+            var kunde2 = Target.loadKunde(1);
+            Assert.AreNotEqual(kunde1, kunde2);
+
+            kunde1.Nachname = "A";
+            kunde2.Nachname = "B";
+
+            Target.updateKunde(kunde1);
+
+            try
+            {
+                Target.updateKunde(kunde2);
+                Assert.Fail("Was able to override!");
+            }
+            catch (LocalOptimisticConcurrencyException<Kunde>)
+            {
+                // Everything OK
+            }
+
+            Assert.AreEqual("A", Target.loadKunde(1).Nachname);
         }
 
         [TestMethod]
         public void UpdateReservationTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var res1 = Target.loadReservation(1);
+            var res2 = Target.loadReservation(1);
+            Assert.AreNotEqual(res1, res2);
+
+            res1.Kunde = Target.loadKunde(1);
+            res2.Kunde = Target.loadKunde(2);
+
+            Target.updateReservation(res1);
+
+            try
+            {
+                Target.updateReservation(res2);
+                Assert.Fail("Was able to override!");
+            }
+            catch (LocalOptimisticConcurrencyException<Reservation>)
+            {
+                // Everything OK
+            }
+
+            Assert.AreEqual(1, Target.loadReservation(1).Kunde.Id);
         }
 
     }
--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -9,7 +9,110 @@ namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
- 
+
+        private void insert<T>(T obj) where T : class
+        {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(obj).State = EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public void update<T>(T obj) where T : class
+        {
+            using (var db = new AutoReservationContext())
+            {
+                try
+                {
+                    db.Entry(obj).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateLocalOptimisticConcurrencyException(db, obj);
+                }
+            }
+        }
+
+        public void delete<T>(T obj) where T : class
+        {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(obj).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+
+        public Auto loadAuto(int index)
+        {
+            using (var db = new AutoReservationContext())
+            {
+                return db.Autos.SingleOrDefault(auto => auto.Id == index);
+            }
+        }
+        public Kunde loadKunde(int index)
+        {
+            using (var db = new AutoReservationContext())
+            {
+                return db.Kunden.SingleOrDefault(kunde => kunde.Id == index);
+            }
+        }
+
+        public Reservation loadReservation(int index)
+        {
+            using (var db = new AutoReservationContext())
+            {
+                return db.Reservationen.SingleOrDefault(reservation => reservation.ReservationsNr == index);
+            }
+        }
+
+
+        public void insertAuto(Auto auto)
+        {
+            insert(auto);
+        }
+
+        public void updateAuto(Auto auto)
+        {
+            update(auto);
+        }
+
+        public void deleteAuto(Auto auto)
+        {
+            delete(auto);
+        }
+
+        public void insertKunde(Kunde customer)
+        {
+            insert(customer);
+        }
+
+        public void updateKunde(Kunde customer)
+        {
+            update(customer);
+        }
+
+        public void deleteKunde(Kunde customer)
+        {
+            delete(customer);
+        }
+
+
+        public void insertReservation(Reservation reservation)
+        {
+            insert(reservation);
+        }
+
+        public void updateReservation( Reservation reservation)
+        {
+            update(reservation);
+        }
+
+        public void deleteReservation(Reservation reservation)
+        {
+            delete(reservation);
+        }
 
         private static LocalOptimisticConcurrencyException<T> CreateLocalOptimisticConcurrencyException<T>(AutoReservationContext context, T entity)
             where T : class
