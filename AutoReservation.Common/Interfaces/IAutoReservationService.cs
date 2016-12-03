c513fccab09e51f2cde1c028b90d22e1877a64d5
jmatj <jonas.matter@bluewin.ch>
2016-12-03 14:52:16 +0100

    implement service layer

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,43 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        List<AutoDto> Autos { [OperationContract] get; }
+        List<KundeDto> Kunden { [OperationContract] get; }
+        List<ReservationDto> Reservationen { [OperationContract] get; }
+
+
+        [OperationContract]
+        AutoDto InsertAuto(AutoDto autoDto);
+
+        [OperationContract]
+        KundeDto InsertKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        ReservationDto InsertReservation(ReservationDto reservationDto);
+
+        [OperationContract]
+        AutoDto UpdateAuto(AutoDto autoDto);
+
+        [OperationContract]
+        KundeDto UpdateKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        ReservationDto UpdateReservation(ReservationDto reservationDto);
+
+        [OperationContract]
+        void DeleteAuto(AutoDto autoDto);
+
+        [OperationContract]
+        void DeleteKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        void DeleteReservation(ReservationDto reservationDto);
+
     }
 }
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,14 +1,100 @@
-﻿using System;
+﻿using AutoReservation.BusinessLayer;
+using AutoReservation.Common.Interfaces;
+using System;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService : IAutoReservationService
     {
 
-        private static void WriteActualMethod()
+        private AutoReservationBusinessComponent autoReservationBusinessComponent = new AutoReservationBusinessComponent();
+
+        public List<AutoDto> Autos
         {
-            Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+            get
+            {
+                List<Auto> autoList = autoReservationBusinessComponent.LoadAllAutos();
+                return autoList.ConvertToDtos();
+            }
+        }
+
+        public List<KundeDto> Kunden
+        {
+            get
+            {
+                List<Kunde> kundeList = autoReservationBusinessComponent.LoadAllKunden();
+                return kundeList.ConvertToDtos();
+            }
+        }
+
+        public List<ReservationDto> Reservationen
+        {
+            get
+            {
+                List<Reservation> reservationList = autoReservationBusinessComponent.LoadAllReservations();
+                return reservationList.ConvertToDtos();
+            }
+        }
+
+        public void DeleteAuto(AutoDto autoDto)
+        {
+            autoReservationBusinessComponent.DeleteAuto(autoDto.ConvertToEntity());
+        }
+
+        public void DeleteKunde(KundeDto kundeDto)
+        {
+            autoReservationBusinessComponent.DeleteKunde(kundeDto.ConvertToEntity());
+        }
+
+        public void DeleteReservation(ReservationDto reservationDto)
+        {
+            autoReservationBusinessComponent.DeleteReservation(reservationDto.ConvertToEntity());
+        }
+
+        public AutoDto InsertAuto(AutoDto autoDto)
+        {
+            Auto auto = autoDto.ConvertToEntity();
+            autoReservationBusinessComponent.InsertAuto(auto);
+            return auto.ConvertToDto();
+        }
+
+        public KundeDto InsertKunde(KundeDto kundeDto)
+        {
+            Kunde kunde = kundeDto.ConvertToEntity();
+            autoReservationBusinessComponent.InsertKunde(kunde);
+            return kunde.ConvertToDto();
+        }
+
+        public ReservationDto InsertReservation(ReservationDto reservationDto)
+        {
+            Reservation reservation = reservationDto.ConvertToEntity();
+            autoReservationBusinessComponent.InsertReservation(reservation);
+            return reservation.ConvertToDto();
+        }
+
+        public AutoDto UpdateAuto(AutoDto autoDto)
+        {
+            Auto auto = autoDto.ConvertToEntity();
+            autoReservationBusinessComponent.UpdateAuto(auto);
+            return auto.ConvertToDto();
+        }
+
+        public KundeDto UpdateKunde(KundeDto kundeDto)
+        {
+            Kunde kunde = kundeDto.ConvertToEntity();
+            autoReservationBusinessComponent.UpdateKunde(kunde);
+            return kunde.ConvertToDto();
+        }
+
+        public ReservationDto UpdateReservation(ReservationDto reservationDto)
+        {
+            Reservation reservation = reservationDto.ConvertToEntity();
+            autoReservationBusinessComponent.UpdateReservation(reservation);
+            return reservation.ConvertToDto();
         }
     }
 }
